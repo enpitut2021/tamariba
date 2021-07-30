d8740e66f2817b3f5df8ba150180d7ec98d3063d
Rmash-2 <s1911423@s.tsukuba.ac.jp>
2021-07-30 14:27:10 +0900

    add package
    Co-authored-by: ShotaImamura <ShotaImamura@users.noreply.github.com>
    Co-authored-by: Jun Nishimura <n.junjun0303@gmail.com>
    Co-authored-by: metafs <metafs@users.noreply.github.com>
    Co-authored-by: hikae <shighten@gmail.com>

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -2,18 +2,43 @@
 // √「Twitterで投稿する」という名前のボタンにする
 // √遊びに行くボタンなどをつくる
 // √そこをクリックしたら投稿するURLを開く
+//
+
+// TODOlist
+// √ dummyTheme 変数を作る
+// √ packageをimport
+// PageTitleを変更する
+// dummyThemeを表示する
 
 // ehika調べた内容
 // URLを開く https://qiita.com/superman9387/items/868ce6ad60b3c177bff1
 // Twitterに投稿するURL https://publish.twitter.com/?buttonType=TweetButton&widget=Button
 //<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
+// var firebaseConfig = {
+//         apiKey: "AIzaSyDRL718fwPKeR9yaoPv8HctlSkHbJBcrCs",
+//         authDomain: "tamariba.firebaseapp.com",
+//         projectId: "tamariba",
+//         storageBucket: "tamariba.appspot.com",
+//         messagingSenderId: "1059122947072",
+//         appId: "1:1059122947072:web:f7917e2de1bc20d097a4e5",
+//         measurementId: "G-9K473SGSNQ",
+//       };
+
 import 'package:flutter/material.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
 }
+
+// final dummyTheme = [
+//   {"themeId": 1, "theme": "課題をしよう", "template": "課題をしよう!"},
+//   {"themeId": 2, "theme": "ご飯を行く", "template": "ご飯に行こう"},
+//   {"themeId": 3, "theme": "遊びに行く", "template": "遊びに行こう"},
+// ];
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
