40829e1d62d0e51525613db3bc145ea3af7faa09
HikaruEgashira <s1811320@gmail.com>
2021-08-02 17:22:19 +0900

    list化

--- lib/page/eventList.dart
+++ lib/page/eventList.dart
@@ -1,10 +1,3 @@
-// final dummyTheme = [
-//   {"themeId": 1, "theme": "課題をしよう", "template": "課題をしよう!"},
-//   {"themeId": 2, "theme": "ご飯を行く", "template": "ご飯に行こう"},
-//   {"themeId": 3, "theme": "遊びに行く", "template": "遊びに行こう"},
-// ];
-
-// stateless widgets
 import 'package:flutter/material.dart';
 import 'package:hello_world/page/event.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -33,6 +26,32 @@ class _MyHomePageState extends State<MyHomePage> {
   final Stream<QuerySnapshot<Map<String, dynamic>>> _eventListStream =
       FirebaseFirestore.instance.collection('event').snapshots();
 
+  Widget _messageItem(String title, String eventId) {
+    return Container(
+      decoration: new BoxDecoration(
+          border:
+              new Border(bottom: BorderSide(width: 1.0, color: Colors.grey))),
+      child: ListTile(
+        title: Text(
+          title,
+          style: TextStyle(color: Colors.black, fontSize: 18.0),
+        ),
+        onTap: () async {
+          await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => EventPage(
+                  eventId: eventId,
+                ),
+              ));
+        }, // タップ
+        onLongPress: () {
+          print("onLongTap called.");
+        }, // 長押し
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
@@ -50,34 +69,12 @@ class _MyHomePageState extends State<MyHomePage> {
         }
 
         return new ListView(
-          children: snapshot.data!.docs.map((DocumentSnapshot document) {
-            Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-            return new ListTile(
-              title: new TextButton(
-                onPressed: () async {
-                  await Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => EventPage(
-                          eventId: document.id,
-                        ),
-                      ));
-                },
-                child: Container(
-                  margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                  padding: const EdgeInsets.all(5.0),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.blue),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Container(
-                    child: Text('${data['title']}'),
-                  ),
-                ),
-              ),
-            );
-          }).toList(),
-        );
+            children: snapshot.data!.docs.map((DocumentSnapshot document) {
+          Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+          return new ListTile(
+            title: _messageItem('${data['title']}', document.id),
+          );
+        }).toList());
       },
     );
   }
